46cbbc6020e9683974b79b2cfec2a9c6689c2ff9
Dan2194 <85521396+Dan2194@users.noreply.github.com>
2023-04-24 19:16:55 +0200

    Language finita

---
+++ js/language.js
@@ -0,0 +1,15 @@
+
+
+var text = ["Choose the language", "選擇語言", "Choisissez la langue", "Alegeți limba", "Виберіть мову", "Scegli la lingua"];
+var counter = 0;
+var elem = document.getElementById("changeText");
+var inst = setInterval(change, 5000);
+
+function change() {
+  elem.innerHTML = text[counter];
+  counter++;
+  if (counter >= text.length) {
+    counter = 0;
+    // clearInterval(inst); // uncomment this if you want to stop refreshing after one cycle
+  }
+}
