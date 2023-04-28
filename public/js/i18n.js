5823ebecd7d0c25d914edcf09e62c664c68f7128
Dan2194 <85521396+Dan2194@users.noreply.github.com>
2023-04-28 17:52:29 +0200

    traduzione primo inserimento (beta)

--- public/js/i18n.js
+++ public/js/i18n.js
@@ -1,109 +1,59 @@
-
-
-
 const defaultLocale = "en";
-
-let locale;
+let locale = localStorage.getItem("locale") || defaultLocale;
+let translations;
 
 // ...
 
 // When the page content is ready...
-
-document.addEventListener("DOMContentLoaded", () => {
-
-  setLocale(defaultLocale);
-
-  bindLocaleSwitcher(defaultLocale);
-
+document.addEventListener("DOMContentLoaded", async () => {
+  translations = await fetchTranslationsFor(locale);
+  setLocale(locale);
+  bindLocaleSwitcher(locale);
 });
 
 // ...
 
 // Whenever the user selects a new locale, we
-
 // load the locale's translations and update
-
 // the page
-
 function bindLocaleSwitcher(initialValue) {
-
-  const switcher =
-
-    document.querySelector("[data-i18n-switcher]");
-
+  const switcher = document.querySelector("[data-i18n-switcher]");
   switcher.value = initialValue;
-
   switcher.onchange = (e) => {
-
     // Set the locale to the selected option[value]
-
     setLocale(e.target.value);
-
+    localStorage.setItem("locale", e.target.value);
   };
-
 }
 
-
 // Load translations for the given locale and translate
-
 // the page to this locale
-
 async function setLocale(newLocale) {
-
   if (newLocale === locale) return;
-
-  const newTranslations =
-
-    await fetchTranslationsFor(newLocale);
-
+  translations = await fetchTranslationsFor(newLocale);
   locale = newLocale;
-
-  translations = newTranslations;
-
   translatePage();
-
 }
 
 // Retrieve translations JSON object for the given
-
 // locale over the network
-
 async function fetchTranslationsFor(newLocale) {
-
   const response = await fetch(`../translations/${newLocale}.json`);
-
   return await response.json();
-
 }
 
 // Replace the inner text of each element that has a
-
 // data-i18n-key attribute with the translation corresponding
-
 // to its data-i18n-key
-
 function translatePage() {
-
-  document
-
-    .querySelectorAll("[data-i18n-key]")
-
-    .forEach(translateElement);
-
+  document.querySelectorAll("[data-i18n-key]").forEach(translateElement);
 }
 
 // Replace the inner text of the given HTML element
-
 // with the translation in the active locale,
-
 // corresponding to the element's data-i18n-key
-
 function translateElement(element) {
-
   const key = element.getAttribute("data-i18n-key");
-
   const translation = translations[key];
-
   element.innerText = translation;
-
 }
